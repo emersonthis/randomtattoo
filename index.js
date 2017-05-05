520465725beb41bd96ad858ac1dba10ad76282b2
Emerson <emerson@wearefine.com>
2017-05-05 14:45:04 -0700

    node works

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ var app = express()
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.set('views', './views');
+
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+    response.render('index', { title: 'Hey', message: 'Hello there!' });
 })
 
 app.listen(app.get('port'), function() {
