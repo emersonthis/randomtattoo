63dfcb250a0f3f164c3bb17198ab02b1396d277c
Emerson <hi@emersonthis.com>
2022-04-12 15:38:39 -0700

    alphabetized lists

--- app.js
+++ app.js
@@ -1,206 +1,205 @@
 var randomTattoo = {
+  agents: [
+    "a beaver",
+    "a bulldog",
+    "a cobra",
+    "a dolphin",
+    "a dragon",
+    "a gorilla",
+    "a grizzly bear",
+    "a hula girl",
+    "a kitty cat",
+    "a magician",
+    "a mermaid",
+    "a mouse",
+    "a ninja turtle",
+    "a panther",
+    "a pinup girl",
+    "a puppydog",
+    "a robot",
+    "a sea monster",
+    "a shark",
+    "a smurf",
+    "a sparrow",
+    "a tiger",
+    "a tyrannosaurus",
+    "a unicorn",
+    "a walrus",
+    "a whale",
+    "a wolf",
+    "an angel",
+    "an eagle",
+    "an octopus",
+    "Daffy Duck",
+    "Keanu Reeves",
+    "Michael Jordan",
+    "Mr. T",
+    "Patrick Swayze",
+    "Wile E. Coyote",
+  ],
 
-    agents : [
-        'Patrick Swayze',
-        'a grizzly bear',
-        'a shark',
-        'a sea monster',
-        'a gorilla',
-        'Michael Jordan',
-        'an angel',
-        'a unicorn',
-        'an eagle',
-        'a tiger',
-        'a robot',
-        'a ninja turtle',
-        'a bulldog',
-        'a hula girl',
-        'a panther',
-        'Wile E. Coyote',
-        'Daffy Duck',
-        'Mr. T',
-        'a wolf',
-        'a pinup girl',
-        'a sparrow',
-        'a mermaid',
-        'a dolphin',
-        'a dragon',
-        'an octopus',
-        'a puppydog',
-        'a kitty cat',
-        'a mouse',
-        'a magician',
-        'a beaver',
-        'a whale',
-        'a smurf',
-        'Keanu Reeves',
+  patients: [
+    "a bible",
+    "a burrito",
+    "a donut",
+    "a hamburger",
+    "a hot dog",
+    "a lighthouse",
+    "a sailboat",
+    "a sausage",
+    "a submarine",
+    "a sushi role",
+    "a taco",
+    "a tall ship",
+    "an avocado",
+    "an El Camino",
+    "the moon",
+    "the planet Earth",
+  ],
 
-    ],
+  accessories: [
+    "a banana",
+    "a bat",
+    "a battleaxe",
+    "a beard",
+    "a beer belly",
+    "a beer",
+    "a bull whip",
+    "a butcher knife",
+    "a cappuchino",
+    "a chainsaw",
+    "a cigar",
+    "a cigarette",
+    "a club",
+    "a crown",
+    "a deck of cards",
+    "a dirt bike",
+    "a flamethrower",
+    "a flower",
+    "a harpoon",
+    "a hot dog",
+    "a lasso",
+    "a magnifying glass",
+    "a martini",
+    "a monocle",
+    "a mullet",
+    "a mustache",
+    "a necktie",
+    "a peach",
+    "a peg leg",
+    "a pipe",
+    "a pocket watch",
+    "a samurai sword",
+    "a sledge hammer",
+    "a slice of pizza",
+    "a sword",
+    "a sword",
+    "a telescope",
+    "a tennis racket",
+    "a toolbox",
+    "a top hat",
+    "a trombone",
+    "a whiskey flask",
+    "an oven mit",
+    "boxing gloves",
+    "jazz hands",
+    "scooter",
+    "sunglasses",
+    "sweater vest",
+    "wings",
+  ],
 
-    patients : [
-        'the moon',
-        'an El Camino',
-        'a sushi role',
-        'a taco',
-        'a burrito',
-        'a lighthouse',
-        'an avocado',
-        'a bible',
-        'a sailboat',
-        'a tall ship',
-        'a submarine',
-        'a donut',
-        'a sausage',
-        'a hot dog',
-        'a hamburger',
-        'the planet Earth'
-    ],
+  transitives: [
+    "arm-wrestling",
+    "biting",
+    "chasing",
+    "climbing",
+    "dancing with",
+    "eating",
+    "fighting",
+    "holding hands with",
+    "holding up",
+    "holding",
+    "jumping over",
+    "karate chopping",
+    "kicking",
+    "kissing",
+    "licking",
+    "pointing at",
+    "punching",
+    "riding on",
+    "shaking hands with",
+    "slam dunking",
+    "slapping",
+    "smashing",
+    "straddling",
+    "tackling",
+  ],
 
-    accessories : [
-        'a sword',
-        'a bat',
-        'a flower',
-        'a slice of pizza',
-        'a trombone',
-        'a pipe',
-        'a top hat',
-        'a mullet',
-        'a beer belly',
-        'a beer',
-        'a chainsaw',
-        'a martini',
-        'a sword',
-        'a tennis racket',
-        'a lasso',
-        'a bull whip',
-        'a club',
-        'a flamethrower',
-        'a samurai sword',
-        'a battleaxe',
-        'a pocket watch',
-        'sunglasses',
-        'a peg leg',
-        'a banana',
-        'a necktie',
-        'a monocle',
-        'a deck of cards',
-        'sweater vest',
-        'a beard',
-        'a mustache',
-        'a peach',
-        'a dirt bike',
-        'scooter',
-        'a hot dog',
-        'jazz hands',
-        'an oven mit',
-        'a sledge hammer',
-        'a butcher knife',
-        'a cappuchino',
-        'a telescope',
-        'a magnifying glass',
-        'a cigarette',
-        'a cigar',
-        'a harpoon',
-        'a toolbox',
-        'wings',
-        'a whiskey flask',
-        'a crown',
-        'boxing gloves'
+  intransitives: [
+    "blowing a kiss",
+    "chilling on a log",
+    "chugging a beer",
+    "crying",
+    "drinking whiskey",
+    "fishing",
+    "flipping the bird",
+    "giving a thumbs up",
+    "high-kicking",
+    "laughing",
+    "laying in a hammock",
+    "moonwalking",
+    "on fire",
+    "parachuting",
+    "riding a Harley Davidson",
+    "rollerblading",
+    "showing some leg",
+    "smiling",
+    "snowboarding",
+    "surfing",
+    "tap dancing",
+    "winking",
+    "winking",
+  ],
 
+  init: function () {
+    var template = randomTattoo.random(randomTattoo.templates);
+    var str = "";
+    for (var i = 0; i < template.length; i++) {
+      str += template[i]() + " ";
+    }
+    return str;
+  },
 
-    ],
+  getAgent: function () {
+    return randomTattoo.random(randomTattoo.agents);
+  },
 
-    transitives : [
-        'punching',
-        'kissing',
-        'licking',
-        'dancing with',
-        'riding on',
-        'smashing',
-        'slam dunking',
-        'eating',
-        'slapping',
-        'holding',
-        'arm-wrestling',
-        'holding up',
-        'karate chopping',
-        'jumping over',
-        'pointing at',
-        'shaking hands with',
-        'straddling',
-        'climbing',
-        'tackling',
-        'biting',
-        'chasing',
-        'fighting',
-        'holding hands with',
-        'kicking'
+  getTransitive: function () {
+    return randomTattoo.random(randomTattoo.transitives);
+  },
 
-    ],
+  getIntransitive: function () {
+    return randomTattoo.random(randomTattoo.intransitives);
+  },
 
-    intransitives : [
-        'surfing',
-        'tap dancing',
-        'giving a thumbs up',
-        'smiling',
-        'winking',
-        'parachuting',
-        'snowboarding',
-        'chilling on a log',
-        'drinking whiskey',
-        'chugging a beer',
-        'high-kicking',
-        'flipping the bird',
-        'winking',
-        'crying',
-        'showing some leg',
-        'fishing',
-        'riding a Harley Davidson',
-        'laying in a hammock',
-        'on fire',
-        'laughing',
-        'blowing a kiss',
-        'moonwalking',
-        'rollerblading'
+  getPatient: function () {
+    return randomTattoo.random(
+      randomTattoo.agents.concat(randomTattoo.patients)
+    );
+  },
 
-    ],
-
-    init : function() {
-        var template = randomTattoo.random(randomTattoo.templates);
-        var str = '';
-        for (var i=0; i < template.length; i++) {
-            str += template[i]() + ' ';
-        }
-        return str;
-    },
-
-    getAgent : function() {
-        return randomTattoo.random(randomTattoo.agents);
-    },
-
-    getTransitive : function() {
-        return randomTattoo.random(randomTattoo.transitives);
-    },
-
-    getIntransitive : function() {
-        return randomTattoo.random(randomTattoo.intransitives);
-    },
-
-    getPatient : function() {
-        return randomTattoo.random( randomTattoo.agents.concat(randomTattoo.patients) );
-    },
-
-    random : function(list) {
-        return list[Math.floor(Math.random()*list.length)];
-    },
-}
+  random: function (list) {
+    return list[Math.floor(Math.random() * list.length)];
+  },
+};
 randomTattoo.templates = [
-    [randomTattoo.getAgent, randomTattoo.getTransitive, randomTattoo.getPatient],
-    [randomTattoo.getAgent, randomTattoo.getIntransitive]
+  [randomTattoo.getAgent, randomTattoo.getTransitive, randomTattoo.getPatient],
+  [randomTattoo.getAgent, randomTattoo.getIntransitive],
 ];
 
-function stopThinking(){
-    document.getElementById('spinner').remove();
-    document.getElementById('idea').append(randomTattoo.init());
+function stopThinking() {
+  document.getElementById("spinner").remove();
+  document.getElementById("idea").append(randomTattoo.init());
 }
 var t = setTimeout(stopThinking, 3000);
